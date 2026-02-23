2fae641fcd70be5f7bc1df037dbfbbcd3bbef8b2
tshidiso-ium <tshidiso.ium@gmail.com>
2026-02-23 22:35:58 +0200

    update

--- src/components/Navbar/mobileNavbar.js
+++ src/components/Navbar/mobileNavbar.js
@@ -6,6 +6,198 @@ import Profile from '../login/Profile';
 
 export default function Navbar({userLoginDetails, changeLiState, logoutClicked }) {
   const [open, setOpen] = useState(false);
+    const [activePan, setActivePan] = useState("Home");
+    const [liHome, setLiHome] = useState({class:"nav-link-active"});
+    const [profileComponent, setProfileComponent] = useState({class:"nav-link"});
+    const [donatePage, setDonatePage] = useState({class:"nav-link"});
+    const [loginComponent, setLoginComponent] = useState({class:"nav-link"});
+    const [liPastPapers, setLiPastPapers] = useState({class:"nav-link"});
+    // const [logoutClicked, setLogoutClicked] = useState(false);
+    const [chatBotComponent, setChatBotComponent] = useState({class:"nav-link"});
+    const [purchaseTokensComponent, setPurchaseTokensComponent] = useState({class:"nav-link"});
+
+  function changeLiStateFunction(event){
+    if(event?.preventDefault)
+    {
+      console.log("preventDefault")
+      event.preventDefault();
+    }
+    console.log(event.target)
+    console.log(event.target.id)
+    switch (event.target.id) {
+    case  "homeLinkClick": 
+      setLiHome(
+        {class:"nav-link-active"}
+      )
+      setProfileComponent(
+        {class:"nav-link"}
+      )
+      setChatBotComponent(
+        {class:"nav-link"}
+      )
+      setDonatePage(
+        {class:"nav-link"}
+      )
+      setPurchaseTokensComponent(
+        {class:"nav-link"}
+      )
+      setLiPastPapers(
+        {class:"nav-link"}
+      )
+      setLoginComponent(
+        {class:"nav-link"}
+      )
+      break;
+    case "pastPapersLinkClick":
+      setLiHome(
+        {class:"nav-link"}
+      )
+      setProfileComponent(
+        {class:"nav-link"}
+      )
+      setLiPastPapers(
+        {class:"nav-link-active"}
+      )
+      setChatBotComponent(
+        {class:"nav-link"}
+      )
+      setDonatePage(
+        {class:"nav-link"}
+      )
+      setPurchaseTokensComponent(
+        {class:"nav-link"}
+      )
+      setLoginComponent(
+        {class:"nav-link"}
+      )
+      break;
+    case "donateLinkClick":
+      setLiHome(
+        {class:"nav-link"}
+      )
+      setChatBotComponent(
+        {class:"nav-link"}
+      )
+      setProfileComponent(
+        {class:"nav-link"}
+      )
+      setPurchaseTokensComponent(
+        {class:"nav-link"}
+      )
+      setDonatePage(
+       {class:"nav-link-active"}
+      )
+      setLoginComponent(
+        {class:"nav-link"}
+      )
+      setLiPastPapers(
+        {class:"nav-link"}
+      )
+      break;
+    case "loginLinkClick":
+      console.log("Login clicked")
+      setLiHome(
+        {class:"nav-link"}
+      )
+      setDonatePage(
+       {class:"nav-link"}
+      )
+      setChatBotComponent(
+        {class:"nav-link"}
+      )
+      setProfileComponent(
+        {class:"nav-link"}
+      )
+      setPurchaseTokensComponent(
+        {class:"nav-link"}
+      )
+      setLoginComponent(
+        {class:"nav-link-active"}
+      )
+      setLiPastPapers(
+        {class:"nav-link"}
+      )
+      setActivePan(
+        "Home"
+      )
+      break;
+    case "chatbotLinkCLick":
+      console.log("Chat Bot clicked")
+      setLiHome(
+        {class:"nav-link"}
+      )
+      setDonatePage(
+        {class:"nav-link"}
+      )
+      setLoginComponent(
+        {class:"nav-link"}
+      )
+      setProfileComponent(
+        {class:"nav-link"}
+      )
+      setPurchaseTokensComponent(
+        {class:"nav-link"}
+      )
+      setChatBotComponent(
+        {class:"nav-link-active"}
+      )
+      setLiPastPapers(
+        {class:"nav-link"}
+      )
+      break;
+    case "profileComponentCLick":
+      console.log("Profile Component Activiated")
+      setLiHome(
+        {class:"nav-link"}
+      )
+      setDonatePage(
+        {class:"nav-link"}
+      )
+      setLoginComponent(
+        {class:"nav-link"}
+      )
+      setChatBotComponent(
+        {class:"nav-link"}
+      )
+      setPurchaseTokensComponent(
+        {class:"nav-link"}
+      )
+      setLiPastPapers(
+        {class:"nav-link"}
+      )
+      setProfileComponent(
+        {class:"nav-link-active"}
+      )
+      break;
+    case "purchasTokensLinkClick":
+      console.log("Puchase Tokens Component Activiated")
+      setLiHome(
+        {class:"nav-link"}
+      )
+      setDonatePage(
+        {class:"nav-link"}
+      )
+      setLoginComponent(
+        {class:"nav-link"}
+      )
+      setChatBotComponent(
+        {class:"nav-link"}
+      )
+      setProfileComponent(
+        {class:"nav-link"}
+      )
+      setLiPastPapers(
+        {class:"nav-link"}
+      )
+      setPurchaseTokensComponent(
+        {class:"nav-link-active"}
+      )
+      break;
+    default:
+      break;
+    }
+  }
+
 
   return (
     <nav className="navbar">
@@ -20,34 +212,36 @@ export default function Navbar({userLoginDetails, changeLiState, logoutClicked }
       </div>
 
       <div className={`nav-links ${open ? "open" : ""}`}>
-        <div className={`nav-link`}>
-          <span href="#Home"  id="homeLinkClick"  onClick={(e)=>{changeLiState(e); setOpen(false)}}>Home</span>
+        <div className={liHome.class}>
+          <span href="#Home"  id="homeLinkClick"  onClick={(e)=>{changeLiStateFunction(e); changeLiState(e); setOpen(false)}}>Home</span>
         </div>
-        <div className={`nav-link`}>
-          <span href="#PastPapers" id="pastPapersLinkClick"  onClick={(e)=>{changeLiState(e); setOpen(false)}}>Exam Papers</span>
+        <div  className={liPastPapers.class}>
+          <span href="#PastPapers" id="pastPapersLinkClick"  onClick={(e)=>{changeLiStateFunction(e); changeLiState(e); setOpen(false)}}>Exam Papers</span>
         </div>
-        <div className={`nav-link`}>
-          <span href="#Donate" id="donateLinkClick" onClick={(e)=>{changeLiState(e); setOpen(false)}}>Donate</span>
+        <div  className={donatePage.class}>
+          <span href="#Donate" id="donateLinkClick" onClick={(e)=>{changeLiStateFunction(e); changeLiState(e); setOpen(false)}}>Donate</span>
         </div>
             {
               userLoginDetails.userLogedIn ? 
                 <>
-                  <div className={`nav-link`}>
-                    <span id="chatbotLinkCLick"  href="#Chatbot" onClick={(e)=>{changeLiState(e); setOpen(false)}}>Chat</span>
+                  <div className={chatBotComponent.class}>
+                    <span id="chatbotLinkCLick"  href="#Chatbot" onClick={(e)=>{changeLiStateFunction(e); changeLiState(e); setOpen(false)}}>Chat</span>
                   </div>
-                  <div className={`nav-link`}>
-                    <span id="purchasTokensLinkClick" href="#puchaseTokens" onClick={(e)=>{changeLiState(e); setOpen(false)}}>Purchase Tokens</span>
+                  <div className={purchaseTokensComponent.class}>
+                    <span id="purchasTokensLinkClick" href="#puchaseTokens" onClick={(e)=>{changeLiStateFunction(e); 
+                      changeLiState(e); setOpen(false)}}>Purchase Tokens</span>
                   </div>
-                  <div className={`nav-link`}>
+                  <div className={profileComponent.class}>
                         <div className="li-a-avator">
                             <Profile user={userLoginDetails}/>
                         </div>
-                        <span href="#Profile" id="profileComponentCLick" onClick={(e)=>{changeLiState(e); setOpen(false)}}>Profile</span>
+                        <span href="#Profile" id="profileComponentCLick" onClick={(e)=>{changeLiStateFunction(e); 
+                          changeLiState(e); setOpen(false)}}>Profile</span>
                   </div>
                 </>
                 : 
                 <div className={`nav-link`}>
-                  <span id="loginLinkClick" onClick={(e)=>{changeLiState(e); setOpen(false)}}>Sign In</span>
+                  <span id="loginLinkClick" onClick={(e)=>{changeLiStateFunction(e); changeLiState(e); setOpen(false)}}>Sign In</span>
                 </div>
             }
       </div>
